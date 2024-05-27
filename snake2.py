a9947c0434ef73850df509448a2f6ca380ecfa4e
oliver <oliver.bueno@correounivalle.edu.co>
2024-05-26 20:42:08 -0500

    agrege la primera parte

---
+++ snake2.py
@@ -0,0 +1,60 @@
+import turtle
+import time
+
+posponer = 0.1
+
+#configuracion de la ventana
+wn = turtle.Screen()
+wn.title("juego de snake")
+wn.bgcolor("black")
+wn.setup(width= 600, height= 600)
+wn.tracer(0)
+
+#cabeza serpiente
+cabeza= turtle.Turtle()
+cabeza.speed(0)
+cabeza.shape("square")
+cabeza.color("white")
+cabeza.penup()
+cabeza.goto(0,0)
+cabeza.direction = "stop"
+
+#funciones
+
+def arriba():
+    cabeza.direction = "up"
+def abajo():
+    cabeza.direction = "down"
+def izquierda():
+    cabeza.direction = "left"
+def derecha():
+    cabeza.direction = "right"
+
+
+def mov():
+    if cabeza.direction == "up":
+        y = cabeza.ycor()
+        cabeza.sety(y + 20)
+    if cabeza.direction == "down":
+        y = cabeza.ycor()
+        cabeza.sety(y - 20)
+    if cabeza.direction == "left":
+        x = cabeza.xcor()
+        cabeza.setx(x - 20)
+    if cabeza.direction == "right":
+        x = cabeza.xcor()
+        cabeza.setx(x + 20)
+
+#teclado
+wn.listen()
+wn.onkeypress(arriba, "Up")
+wn.onkeypress(abajo, "Down")
+wn.onkeypress(izquierda,"Left")
+wn.onkeypress(derecha,"Right")
+    
+
+while True:
+    wn.update()
+    
+    mov()
+    time.sleep(posponer)
